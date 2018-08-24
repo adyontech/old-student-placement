d9d24926ba5020bfb2ae07d7ffc215456e1f92d6
aadii104 <sharmaadi104@gmail.com>
2018-08-24 20:34:23 +0530

    sidebar done.

--- src/components/innerPages/innerPages.vue
+++ src/components/innerPages/innerPages.vue
@@ -1,29 +1,56 @@
 <template>
     <v-app>
         <div style="background-color: #f2f3f4">
-            <!--<navbar></navbar>
-            <router-view></router-view>
-            <footers></footers>
-        </div> -->
-            <v-navigation-drawer v-model="drawer" clipped fixed app>
-                <v-list dense>
+
+            <v-navigation-drawer right v-model="drawer" clipped fixed app v-if=" $vuetify.breakpoint.smAndDown">
+                <v-toolbar flat>
+                    <v-list>
+                        <v-list-tile>
+                            <v-list-tile-title class="title pl-3">
+                                Placement Control
+                            </v-list-tile-title>
+                        </v-list-tile>
+                    </v-list>
+                </v-toolbar>
+                <v-list text-xs-center>
                     <v-list-tile>
                         <v-list-tile-action>
-                            <v-icon>dashboard</v-icon>
+                            <v-icon>home</v-icon>
                         </v-list-tile-action>
-                        <v-list-tile-content>
-                            <v-list-tile-title>Dashboard</v-list-tile-title>
-                        </v-list-tile-content>
+                        <v-list-tile-title>Home</v-list-tile-title>
+                    </v-list-tile>
+
+                    <v-list-tile>
+                        <v-list-tile-action>
+                            <v-icon>book</v-icon>
+                        </v-list-tile-action>
+                        <v-list-tile-title>Companies</v-list-tile-title>
+                    </v-list-tile>
+                    <v-list-tile>
+                        <v-list-tile-action>
+                            <v-icon>book</v-icon>
+                        </v-list-tile-action>
+                        <v-list-tile-title>Jobs</v-list-tile-title>
                     </v-list-tile>
                     <v-list-tile>
                         <v-list-tile-action>
                             <v-icon>settings</v-icon>
                         </v-list-tile-action>
-                        <v-list-tile-content>
-                            <v-list-tile-title>Settings</v-list-tile-title>
-                        </v-list-tile-content>
+                        <v-list-tile-title>Profile</v-list-tile-title>
                     </v-list-tile>
                 </v-list>
+                <v-divider />
+                <div text-xs-center>
+                    <v-list-tile>
+                        <v-list-tile-action>
+                            <v-icon>help</v-icon>
+                        </v-list-tile-action>
+                        <v-list-tile-title>Help</v-list-tile-title>
+                    </v-list-tile>
+                    <v-list-tile class="ml-5 pr-2">
+                        <v-btn color="info">Log Out</v-btn>
+                    </v-list-tile>
+                </div>
             </v-navigation-drawer>
             <navbar v-on:toggleDrawerValue="drawerSideToggle($event)"></navbar>
             <router-view></router-view>
@@ -37,7 +64,7 @@ import navbar from '../shared/navbar.vue';
 import footers from '../shared/footer.vue';
 export default {
   data: () => ({
-    drawer: false,
+    drawer: true,
   }),
   components: {
     navbar: navbar,
@@ -45,8 +72,7 @@ export default {
   },
   methods: {
     drawerSideToggle(value) {
-      console.log(value);
-      this.drawer = value;
+      this.drawer = !this.drawer;
     },
   },
 };
--- src/components/shared/navbar.vue
+++ src/components/shared/navbar.vue
@@ -1,74 +1,70 @@
  <template>
-  <div class="px-5 white">
-    <div>
-      <v-toolbar :height="56" flat :ripple="false" color="white " class="pl-5">
+  <div class="white" :class="{'px-5': $vuetify.breakpoint.mdAndUp}">
 
-        <v-toolbar-title class="blue--text ml-5 pt-3">
-          <router-link color="primary--text" to="/" style="text-decoration:none" exact> <img src="./../../assets/images/outer-img/favicon-32x32.png" alt="" srcset=""> </router-link>
-        </v-toolbar-title>
-        <v-toolbar-title class="black--tex ml-4 pt-3" v-if="$vuetify.breakpoint.mdAndUp" style="height: 55px" :class="{'border-bott': isHome}">
-          <router-link color="primary--text" to="/" class="caption pc_nav" style="text-decoration:none;" exact> HOME </router-link>
-        </v-toolbar-title>
-        <v-toolbar-title class="black--tex ml-4 pt-3" v-if="$vuetify.breakpoint.mdAndUp" style="height: 55px" :class="{'border-bott': isProfile}">
-          <router-link color="primary--text" to="/profile" class="caption pc_nav" style="text-decoration:none;" exact> PROFILE </router-link>
-        </v-toolbar-title>
-        <v-toolbar-title class="black--text ml-4 pt-3" v-if="$vuetify.breakpoint.mdAndUp" style="height: 55px" :class="{'border-bott': isJob}">
-          <router-link color="primary--text" to="/jobs" class="caption" style="text-decoration:none" exact> JOBS </router-link>
-        </v-toolbar-title>
-        <v-toolbar-title class="black--text ml-4 pt-3" v-if="$vuetify.breakpoint.mdAndUp" style="height: 55px" :class="{'border-bott': isCompany}">
-          <router-link color="primary--text" to="/company" class="caption" style="text-decoration:none" exact> COMPANIES </router-link>
-        </v-toolbar-title>
-        <v-toolbar-side-icon @click="toggleDrawer"></v-toolbar-side-icon>
-        <v-spacer></v-spacer>
-        <v-toolbar-items class="mr-5">
-          <span class="pt-3 mr-4">
-            <div class="" style="border: 1px solid #d3cfcf">
-              <v-badge color="red">
-                <span slot="badge">2</span>
-                <v-icon medium color="grey">
-                  notification_important
-                </v-icon>
-              </v-badge>
-            </div>
-          </span>
+    <v-toolbar :height="56" flat :ripple="false" color="white " :class="{'pl-5': $vuetify.breakpoint.mdAndUp}">
 
-          <!-- <v-avatar size="34" tile>
-                  <img src="https://cdn.vuetifyjs.com/images/john.jpg " alt="John ">
-                </v-avatar> -->
-          <span class="pt-3 mr-4">
-            <div class="">
-              <v-menu absolute offset-y>
-                <v-avatar size="34" tile slot="activator">
-                  <img src="https://cdn.vuetifyjs.com/images/john.jpg " alt="John ">
-                </v-avatar>
-                <v-list class="py-0 px-0">
-                  <v-list-tile :to="{path:'/edit/profile'}">
-                    <v-list-tile-avatar class="pr-2">
-                      <v-icon>person</v-icon>
-                    </v-list-tile-avatar>
-                    Profile
-                  </v-list-tile>
-                  <v-list-tile :to="{path:'/edit/profile'}">
-                    <v-list-tile-avatar class="pr-2">
-                      <v-icon>person</v-icon>
-                    </v-list-tile-avatar>
-                    Help
-                  </v-list-tile>
-                  <v-divider></v-divider>
-                  <v-list-tile class="red--text text--accent-2" @click="logout()">
-                    <v-list-tile-avatar class="pr-2">
-                      <v-icon color="red accent-2">power_settings_new</v-icon>
-                    </v-list-tile-avatar>
-                    Logout
-                  </v-list-tile>
-                </v-list>
-              </v-menu>
-            </div>
-          </span>
+      <v-toolbar-title class="blue--text pt-3" :class="{'ml-5': $vuetify.breakpoint.mdAndUp}">
+        <router-link color="primary--text" to="/" style="text-decoration:none" exact> <img src="./../../assets/images/outer-img/favicon-32x32.png" alt="" srcset=""> </router-link>
+      </v-toolbar-title>
+      <v-toolbar-title class="black--tex ml-4 pt-3" v-if="$vuetify.breakpoint.mdAndUp" style="height: 55px" :class="{'border-bott': isHome}">
+        <router-link color="primary--text" to="/" class="caption pc_nav" style="text-decoration:none;" exact> HOME </router-link>
+      </v-toolbar-title>
+      <v-toolbar-title class="black--tex ml-4 pt-3" v-if="$vuetify.breakpoint.mdAndUp" style="height: 55px" :class="{'border-bott': isProfile}">
+        <router-link color="primary--text" to="/profile" class="caption pc_nav" style="text-decoration:none;" exact> PROFILE </router-link>
+      </v-toolbar-title>
+      <v-toolbar-title class="black--text ml-4 pt-3" v-if="$vuetify.breakpoint.mdAndUp" style="height: 55px" :class="{'border-bott': isJob}">
+        <router-link color="primary--text" to="/jobs" class="caption" style="text-decoration:none" exact> JOBS </router-link>
+      </v-toolbar-title>
+      <v-toolbar-title class="black--text ml-4 pt-3" v-if="$vuetify.breakpoint.mdAndUp" style="height: 55px" :class="{'border-bott': isCompany}">
+        <router-link color="primary--text" to="/company" class="caption" style="text-decoration:none" exact> COMPANIES </router-link>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="mr-5">
+        <span class="pt-3 mr-4">
+          <div class="" style="border: 1px solid #d3cfcf">
+            <v-badge color="red">
+              <span slot="badge">2</span>
+              <v-icon medium color="grey">
+                notification_important
+              </v-icon>
+            </v-badge>
+          </div>
+        </span>
 
-        </v-toolbar-items>
-      </v-toolbar>
-    </div>
+        <v-toolbar-side-icon @click="toggleDrawer" v-if=" $vuetify.breakpoint.smAndDown"></v-toolbar-side-icon>
+
+        <span class="pt-3 mr-4" v-if=" $vuetify.breakpoint.mdAndUp">
+          <div class="">
+            <v-menu absolute offset-y>
+              <v-avatar size="34" tile slot="activator">
+                <img src="https://cdn.vuetifyjs.com/images/john.jpg " alt="John ">
+              </v-avatar>
+              <v-list class="py-0 px-0">
+                <v-list-tile :to="{path:'/edit/profile'}">
+                  <v-list-tile-avatar class="pr-2">
+                    <v-icon>person</v-icon>
+                  </v-list-tile-avatar>
+                  Profile
+                </v-list-tile>
+                <v-list-tile :to="{path:'/edit/profile'}">
+                  <v-list-tile-avatar class="pr-2">
+                    <v-icon>person</v-icon>
+                  </v-list-tile-avatar>
+                  Help
+                </v-list-tile>
+                <v-divider></v-divider>
+                <v-list-tile class="red--text text--accent-2" @click="logout()">
+                  <v-list-tile-avatar class="pr-2">
+                    <v-icon color="red accent-2">power_settings_new</v-icon>
+                  </v-list-tile-avatar>
+                  Logout
+                </v-list-tile>
+              </v-list>
+            </v-menu>
+          </div>
+        </span>
+      </v-toolbar-items>
+    </v-toolbar>
   </div>
 
 </template>
@@ -121,7 +117,6 @@ export default {
   methods: {
     toggleDrawer() {
       this.drawerState = !this.drawerState;
-      console.log('sa');
       this.$emit('toggleDrawerValue', this.drawerState);
     },
   },
