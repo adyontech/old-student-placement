b1fd2b59b51acc84fc35fb8092020eadbefc0324
aadii104 <sharmaadi104@gmail.com>
2018-09-10 23:22:51 +0530

    updated profile component.

---
+++ src/components/innerPages/newsFeed/profileCard.vue
@@ -0,0 +1,14 @@
+<template>
+    <v-card flat>
+        <v-card flat img="https://cdn.vuetifyjs.com/images/cards/docks.jpg" height="150" class="mx-4 mt-4" />
+        <v-card flat>
+            <v-card-text>SRM UNVIERSITY</v-card-text>
+            <v-divider></v-divider>
+            <v-layout row>
+                <v-card-text>John White</v-card-text>
+                <v-spacer></v-spacer>
+                <v-icon class="mr-3">edit</v-icon>
+            </v-layout>
+        </v-card>
+    </v-card>
+</template>
